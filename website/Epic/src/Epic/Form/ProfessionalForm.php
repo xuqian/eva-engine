2e72c81ff9e10ec583638286b14a2fc179cec7f4
ctqh <ctqh@easthv.com>
2012-09-14 20:37:20 +0800

    zhuan ye

---
+++ website/Epic/src/Epic/Form/ProfessionalForm.php
@@ -0,0 +1,384 @@
+<?php
+/**
+ * EvaEngine
+ *
+ * @link      https://github.com/AlloVince/eva-engine
+ * @copyright Copyright (c) 2012 AlloVince (http://avnpc.com/)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Eva_Api.php
+ * @author    AlloVince
+ */
+
+namespace Epic\Form;
+
+/**
+ * Eva Form will automatic combination form Elements & Validators & Filters
+ * Also allow add sub forms and unit validate
+ * 
+ * @category   Eva
+ * @package    Eva_Form
+ */
+class ProfessionalForm extends \Eva\Form\RestfulForm
+{
+    protected $subFormGroups = array(
+        'default' => array(
+            'ProfessionalProfile' => 'Epic\Form\ProfessionalProfileForm',
+        )
+    );
+
+    /**
+     * Form basic elements
+     *
+     * @var array
+     */
+    protected $baseElements = array (
+        'id' => array (
+            'name' => 'id',
+            'type' => 'hidden',
+            'options' => array (
+                'label' => 'Id',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'screenName' => array (
+            'name' => 'screenName',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Title',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'gender' => array (
+            'name' => 'gender',
+            'type' => 'select',
+            'options' => array (
+                'label' => 'Gender',
+                'value_options' => array (
+                    array (
+                        'label' => 'Select Gender',
+                        'value' => 'other',
+                    ),
+                    array (
+                        'label' => 'Male',
+                        'value' => 'male',
+                    ),
+                    array (
+                        'label' => 'Female',
+                        'value' => 'female',
+                    ),
+                    array (
+                        'label' => 'Secret',
+                        'value' => 'other',
+                    ),
+                ),
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'email' => array (
+            'name' => 'email',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Email Address',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+    );
+
+    /**
+     * Form basic Validators
+     *
+     * @var array
+     */
+    protected $baseFilters = array (
+        'id' => array (
+            'name' => 'id',
+            'required' => false,
+            'filters' => array (
+            ),
+            'validators' => array (
+                'notEmpty' => array (
+                    'name' => 'NotEmpty',
+                    'options' => array (
+                    ),
+                ),
+            ),
+        ),
+        'userName' => array (
+            'name' => 'userName',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'notEmpty' => array (
+                    'name' => 'NotEmpty',
+                    'options' => array (
+                    ),
+                ),
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '128',
+                    ),
+                ),
+            ),
+        ),
+        'email' => array (
+            'name' => 'email',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '320',
+                    ),
+                ),
+            ),
+        ),
+        'status' => array (
+            'name' => 'status',
+            'required' => true,
+            'filters' => array (
+            ),
+            'validators' => array (
+            ),
+        ),
+        'screenName' => array (
+            'name' => 'screenName',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '128',
+                    ),
+                ),
+            ),
+        ),
+        'firstName' => array (
+            'name' => 'firstName',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '20',
+                    ),
+                ),
+            ),
+        ),
+        'lastName' => array (
+            'name' => 'lastName',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '20',
+                    ),
+                ),
+            ),
+        ),
+        'password' => array (
+            'name' => 'password',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '128',
+                    ),
+                ),
+            ),
+        ),
+        'oldPassword' => array (
+            'name' => 'oldPassword',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '128',
+                    ),
+                ),
+            ),
+        ),
+        'gender' => array (
+            'name' => 'gender',
+            'required' => false,
+            'filters' => array (
+            ),
+            'validators' => array (
+                'inArray' => array (
+                    'name' => 'InArray',
+                    'options' => array (
+                        'haystack' => array (
+                            'male',
+                            'female',
+                            'other',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        'avatar' => array (
+            'name' => 'avatar',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '255',
+                    ),
+                ),
+            ),
+        ),
+        'timezone' => array (
+            'name' => 'timezone',
+            'required' => false,
+            'callback' => 'getTimezones',
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '64',
+                    ),
+                ),
+            ),
+        ),
+        'language' => array (
+            'name' => 'language',
+            'required' => false,
+            'filters' => array (
+                'stripTags' => array (
+                    'name' => 'StripTags',
+                ),
+                'stringTrim' => array (
+                    'name' => 'StringTrim',
+                ),
+            ),
+            'validators' => array (
+                'stringLength' => array (
+                    'name' => 'StringLength',
+                    'options' => array (
+                        'max' => '10',
+                    ),
+                ),
+            ),
+        ),
+        'onlineStatus' => array (
+            'name' => 'onlineStatus',
+            'required' => false,
+            'filters' => array (
+            ),
+            'validators' => array (
+                'inArray' => array (
+                    'name' => 'InArray',
+                    'options' => array (
+                        'haystack' => array (
+                            'online',
+                            'busy',
+                            'invisible',
+                            'offline',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    );
+
+    public function getLanguages($element)
+    {
+        $translator = \Eva\Api::_()->getServiceManager()->get('translator');
+        $locale = $translator->getLocale();
+        $languages = \Eva\Locale\Data::getList($locale, 'language');
+        $element['options']['value_options'] = $languages;
+        return $element;
+    }
+
+    public function getTimezones($element)
+    {
+        $translator = \Eva\Api::_()->getServiceManager()->get('translator');
+        $locale = $translator->getLocale();
+        $languages = \Eva\Locale\Data::getList($locale, 'citytotimezone');
+        $element['options']['value_options'] = $languages;
+        return $element;
+    }
+}
---
+++ website/Epic/src/Epic/Form/ProfessionalProfileForm.php
@@ -0,0 +1,293 @@
+<?php
+/**
+ * EvaEngine
+ *
+ * @link      https://github.com/AlloVince/eva-engine
+ * @copyright Copyright (c) 2012 AlloVince (http://avnpc.com/)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Eva_Api.php
+ * @author    AlloVince
+ */
+
+namespace Epic\Form;
+
+/**
+ * Eva Form will automatic combination form Elements & Validators & Filters
+ * Also allow add sub forms and unit validate
+ * 
+ * @category   Eva
+ * @package    Eva_Form
+ */
+class ProfessionalProfileForm extends \Eva\Form\RestfulForm
+{
+    /**
+     * Form basic elements
+     *
+     * @var array
+     */
+    protected $baseElements = array (
+        'user_id' => array (
+            'name' => 'user_id',
+            'type' => 'hidden',
+            'options' => array (
+                'label' => 'User_id',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'firstName' => array (
+            'name' => 'firstName',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'First Name',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'lastName' => array (
+            'name' => 'lastName',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Last Name',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'birthday' => array (
+            'name' => 'birthday',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Birthday',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'country' => array (
+            'name' => 'country',
+            'type' => 'select',
+            'callback' => 'getCountries',
+            'options' => array (
+                'label' => 'Country',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'hobbies' => array (
+            'name' => 'hobbies',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Hobbies',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'bio' => array (
+            'name' => 'bio',
+            'type' => 'textarea',
+            'options' => array (
+                'label' => 'Few	words about your yourself',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'school' => array (
+            'name' => 'school',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'School',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'degree' => array (
+            'name' => 'degree',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Degree',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'industry' => array (
+            'name' => 'industryIndustrie',
+            'type' => 'select',
+            'options' => array (
+                'label' => 'Industry',
+                'value_options' => array (
+                    array (
+                        'label' => 'Food, Wine and Spirit Production',
+                        'value' => 'Food, Wine and Spirit Production',
+                    ),
+                    array (
+                        'label' => 'Hospitality',
+                        'value' => 'Hospitality',
+                    ),
+                    array (
+                        'label' => 'Restaurant, bar and club',
+                        'value' => 'Restaurant, bar and club',
+                    ),
+                    array (
+                        'label' => 'Distribution and Trade',
+                        'value' => 'Distribution and Trade',
+                    ),
+                    array (
+                        'label' => 'Media and Press',
+                        'value' => 'Media and Press',
+                    ),
+                    array (
+                        'label' => 'Marketing and Communication',
+                        'value' => 'Marketing and Communication',
+                    ),
+                    array (
+                        'label' => 'Consultancy',
+                        'value' => 'Consultancy',
+                    ),
+                    array (
+                        'label' => 'Educational Institution',
+                        'value' => 'Educational Institution',
+                    ),
+                    array (
+                        'label' => 'Research and Development',
+                        'value' => 'Research and Development',
+                    ),
+                    array (
+                        'label' => 'Other',
+                        'value' => 'Other',
+                    ),
+                ),
+            ),
+        ),
+        'phoneBusiness' => array (
+            'name' => 'phoneBusiness',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Phone',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'address' => array (
+            'name' => 'address',
+            'type' => 'text',
+            'options' => array (
+                'label' => 'Address',
+            ),
+            'attributes' => array (
+                'value' => '',
+            ),
+        ),
+        'howCouldWehelpYou' => array (
+            'name' => 'howCouldWehelpYou',
+            'type' => 'multiCheckbox',
+            'options' => array (
+                'label' => 'How could we help you?',
+                'value_options' => array (
+                    array (
+                        'label' => 'Jobs and Intern Opportunities',
+                        'value' => 'Jobs and Intern Opportunities',
+                    ),
+                    array (
+                        'label' => 'Social Network Opportunities',
+                        'value' => 'Social Network Opportunities',
+                    ),
+                    array (
+                        'label' => 'Others',
+                        'value' => 'Others',
+                    ),
+                ),
+            ),
+        ),
+        'yourExpertise' => array (
+            'name' => 'yourExpertise',
+            'type' => 'multiCheckbox',
+            'options' => array (
+                'label' => 'Your Expertise',
+                'value_options' => array (
+                    array (
+                        'label' => 'Chef, Sommelier, Mixologist barman',
+                        'value' => 'Chef, Sommelier, Mixologist barman',
+                    ),
+                    array (
+                        'label' => 'hospitality',
+                        'value' => 'hospitality',
+                    ),
+                    array (
+                        'label' => 'Restaurant and bar management',
+                        'value' => 'Restaurant and bar management',
+                    ),
+                    array (
+                        'label' => 'Distribution, Logistics, and trade',
+                        'value' => 'Distribution, Logistics, and trade',
+                    ),
+                    array (
+                        'label' => 'Public Communication',
+                        'value' => 'Public Communication',
+                    ),
+                    array (
+                        'label' => 'Marketing and Promotion',
+                        'value' => 'Marketing and Promotion',
+                    ),
+                    array (
+                        'label' => 'Events Planning',
+                        'value' => 'Events Planning',
+                    ),
+                    array (
+                        'label' => 'Consultancy',
+                        'value' => 'Consultancy',
+                    ),
+                    array (
+                        'label' => 'Financial Service(seed funding, venture capital. Insurance)',
+                        'value' => 'Financial Service(seed funding, venture capital. Insurance)',
+                    ),
+                    array (
+                        'label' => 'Design',
+                        'value' => 'Design',
+                    ),
+                    array (
+                        'label' => 'Photographing',
+                        'value' => 'Photographing',
+                    ),
+                    array (
+                        'label' => 'Creative Writing',
+                        'value' => 'Creative Writing',
+                    ),
+                    array (
+                        'label' => 'Journalism',
+                        'value' => 'Journalism',
+                    ),
+                    array (
+                        'label' => 'Legal Issues',
+                        'value' => 'Legal Issues',
+                    ),
+                    array (
+                        'label' => 'Academia and Research',
+                        'value' => 'Academia and Research',
+                    ),
+                    array (
+                        'label' => 'Others',
+                        'value' => 'Others',
+                    ),
+                ),
+            ),
+        ),
+    );
+
+    public function getCountries($element)
+    {
+        $translator = \Eva\Api::_()->getServiceManager()->get('translator');
+        $locale = $translator->getLocale();
+        $countries = \Eva\Locale\Data::getList($locale, 'territory');
+        $element['options']['value_options'] = $countries;
+        return $element;
+    }
+}
