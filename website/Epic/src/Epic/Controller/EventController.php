e0247a7d29d5a7296004248f11293e5fa6e1d6bb
xuqian <xuqian@easthv.com>
2012-12-13 17:04:11 +0800

    event activity

--- website/Epic/src/Epic/Controller/EventController.php
+++ website/Epic/src/Epic/Controller/EventController.php
@@ -114,8 +114,16 @@ class EventController extends ActionController
             'id' => $user['id'],
         ));
 
+
+        list($items, $paginator) = $this->forward()->dispatch('FeedController', array(
+            'action' => 'index',
+            'user_id' => $user['id'],
+        ));
+
         $view = new ViewModel(array(
             'item' => $item,
+            'items' => $items,
+            'paginator' => $paginator,
         ));
         return $view; 
     }
