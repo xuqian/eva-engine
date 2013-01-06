205f6999776ad09c7e2284401b7655c75df1e70a
xuqian <xuqian@easthv.com>
2013-01-06 11:48:38 +0800

    new index

--- website/Yicheng/assets/javascripts/app.js
+++ website/Yicheng/assets/javascripts/app.js
@@ -34,9 +34,10 @@ $(document).ready(function(){
 	$('#intro').parallax("50%", 0.1);
 	$('#second').parallax("50%", 0.1);
 	$('.bg').parallax("50%", 0.4);
-	$('#third').parallax("50%", 0.3);
-	$('#fourth').parallax("50%", 3);
+	$('#third').parallax("50%", 0.1);
+	$('#fourth').parallax("50%", 0.2);
 	$('#fifth').parallax("50%", 0.2);
+	$('#article, #overview, #album').parallax("50%", 0.2);
 
 
 
